c3f33191d9f504e03021c6aaae7cc92942c2177a
coockieMonster88 <pajura_pawel@o2.pl>
2018-02-20 11:11:19 +0100

    Zadanie 13.2

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -1,12 +1,11 @@
-package com.kodilla.kodilla.hibernate;
+package com.kodilla.hibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KodillaHibernateApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(KodillaHibernateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernateApplication.class,args);
+    }
 }
